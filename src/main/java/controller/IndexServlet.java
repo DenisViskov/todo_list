b41416e619f99117273439ee96e9edc29610b8be
DenisViskov <DenisViskov16@gmail.com>
2020-09-21 00:13:20 +0300

    update

--- src/main/java/controller/IndexServlet.java
+++ src/main/java/controller/IndexServlet.java
@@ -1,11 +1,17 @@
 package controller;
 
+import model.Task;
+import persistence.HbStore;
+import persistence.Store;
+
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.sql.Timestamp;
+import java.time.LocalDateTime;
 
 /**
  * @author Денис Висков
@@ -14,10 +20,32 @@ import java.io.IOException;
  */
 @WebServlet("/index")
 public class IndexServlet extends HttpServlet {
+
+    /**
+     * Storage
+     */
+    private Store store;
+
+    @Override
+    public void init() throws ServletException {
+        store = new HbStore();
+    }
+
     @Override
     protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
         req.setCharacterEncoding("UTF-8");
         String name = req.getParameter("name");
         String description = req.getParameter("description");
+        Task task = new Task(0, name, description, Timestamp.valueOf(LocalDateTime.now()), false);
+        store.add(task);
+    }
+
+    @Override
+    public void destroy() {
+        try {
+            store.close();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 }
---
+++ src/main/java/model/Task.java
@@ -0,0 +1,107 @@
+package model;
+
+import javax.persistence.*;
+import java.sql.Timestamp;
+import java.util.Objects;
+
+/**
+ * Class is a task
+ *
+ * @author Денис Висков
+ * @version 1.0
+ * @since 20.09.2020
+ */
+@Entity
+@Table(name = "tasks")
+public class Task {
+    /**
+     * ID
+     */
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+    /**
+     * Name
+     */
+    private String name;
+    /**
+     * Description
+     */
+    private String description;
+    /**
+     * Time created
+     */
+    private Timestamp created;
+    /**
+     * Done
+     */
+    private boolean done;
+
+    public Task() {
+    }
+
+    public Task(int id, String name, String description, Timestamp created, boolean done) {
+        this.id = id;
+        this.name = name;
+        this.description = description;
+        this.created = created;
+        this.done = done;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public Timestamp getCreated() {
+        return created;
+    }
+
+    public void setCreated(Timestamp created) {
+        this.created = created;
+    }
+
+    public boolean isDone() {
+        return done;
+    }
+
+    public void setDone(boolean done) {
+        this.done = done;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Task task = (Task) o;
+        return id == task.id &&
+                done == task.done &&
+                Objects.equals(name, task.name) &&
+                Objects.equals(description, task.description) &&
+                Objects.equals(created, task.created);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, name, description, created, done);
+    }
+}
