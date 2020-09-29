6c815a230dfbb804274f9b58b0be603124654678
DenisViskov <denisviskov16@gmail.com>
2020-09-29 20:56:20 +0300

    update

---
+++ src/main/java/controller/SignAnswerGenerator.java
@@ -0,0 +1,39 @@
+package controller;
+
+import org.json.JSONObject;
+
+/**
+ * @author Денис Висков
+ * @version 1.0
+ * @since 29.09.2020
+ */
+public class SignAnswerGenerator implements Answer<JSONObject> {
+
+    /**
+     * Operation
+     */
+    private volatile boolean lastOperation;
+
+    @Override
+    public synchronized JSONObject toFormAnswer() {
+        JSONObject json = new JSONObject();
+        if (lastOperation) {
+            json.put("answer", true);
+        }
+        if (!lastOperation) {
+            json.put("answer", false);
+        }
+        lastOperation = false;
+        return json;
+    }
+
+    @Override
+    public synchronized boolean isLastOperation() {
+        return lastOperation;
+    }
+
+    @Override
+    public synchronized void setLastOperation(boolean lastOperation) {
+        this.lastOperation = lastOperation;
+    }
+}
